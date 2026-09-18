3e3c2358bf87224515d320fd97433b0814330a95
Jessica <maytajessica73@gmail.com>
2026-09-18 14:07:45 -0300

    feat: Adicionar testes para listagem de produtos com e sem login

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
 from app.models.produto import Produto 
 from app.models.categoria import Categoria
 
+#Teste para verificar se a tela funciona!
 def test_listar_produtos_retorna_200(cliente):
 
     resposta = cliente.get("/produtos/")
@@ -8,3 +9,15 @@ def test_listar_produtos_retorna_200(cliente):
     # 200 = ok, a página carregou sem erro.
 
     assert resposta.status_code == 200
+
+# Teste a tela de produtos sem o login
+def test_listagem_produtos_sem_login():
+    from fastapi.testclient import TestClient
+    from app.main import app
+
+    cliente_sem_login = TestClient(app)
+
+    resposta = cliente_sem_login.get("/produtos/")
+
+    assert resposta.status_code == 401
+
