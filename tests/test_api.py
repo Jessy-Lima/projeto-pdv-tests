dee3f6643a8ce78b00a249d1dad0fc87deac7f45
Jessica <maytajessica73@gmail.com>
2026-09-25 14:10:13 -0300

    refactor: Melhorar formatação e comentários nos testes de listagem de produtos

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,13 @@
 from app.models.produto import Produto 
 from app.models.categoria import Categoria
+from app.models.cliente import Cliente
 
-#Teste para verificar se a tela funciona!
+# Teste para verificar se a tela funciona!
 def test_listar_produtos_retorna_200(cliente):
 
     resposta = cliente.get("/produtos/")
 
     # 200 = ok, a página carregou sem erro.
-
     assert resposta.status_code == 200
 
 # Teste a tela de produtos sem o login
@@ -28,12 +28,42 @@ def test_verificar_produto_criando_com_sucesso(cliente, db_session_test):
     db_session_test.add(categoria)
     db_session_test.commit()
 
-    #Criando um produto para teste
-    produto = Produto(nome="Boné Aba Reta", preco=129.90, estoque_atual=50, categoria_id=categoria.id)
+    # Criando um produto para teste
+    produto = Produto(
+        nome="Boné Aba Reta",
+        preco=129.90,
+        estoque_atual=50,
+        categoria_id=categoria.id
+    )
+
     db_session_test.add(produto)
     db_session_test.commit()
 
     resposta = cliente.get("/produtos/")
 
-    #Teste se existe o Boné  na listagem de produtos
+    # Teste se existe o Boné na listagem de produtos
     assert "Boné Aba Reta" in resposta.text
+
+# Testar se a busca retorna somente os produtos filtrados
+def test_listar_produtos_filtrado_por_busca(cliente, db_session_test):
+
+    produto = Produto(
+        nome="Camisa Nike",
+        preco=99.90,
+        estoque_atual=500
+    )
+
+    produto1 = Produto(
+        nome="Caneca Harry Potter",
+        preco=139.90,
+        estoque_atual=60
+    )
+
+    db_session_test.add(produto)
+    db_session_test.add(produto1)
+    db_session_test.commit()
+
+    resposta = cliente.get("/produtos", params={"busca": "Harry"})
+
+    assert "Caneca Harry Potter" in resposta.text
+    assert "Camisa Nike" not in resposta.text
